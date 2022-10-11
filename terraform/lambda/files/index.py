1c16f485bf4f728dd86afb6f29a499af58af8025
unknown <daniel-mcdermott@hotmail.com>
2022-10-11 13:41:05 +0100

    removed lambda files

--- terraform/lambda/files/index.py
+++
@@ -1,8 +0,0 @@
-# index.py
-
-# Declaring the Python Function lambda_handler with event paramater
-def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
